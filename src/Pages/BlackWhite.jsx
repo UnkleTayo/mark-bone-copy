21482ece99fd8667034276c832420239dfa7df09
Adetayo Akinsanya <akinsanyaadetayo@gmail.com>
2021-11-20 14:43:56 +0100

    Fixes and improvement

--- src/Pages/BlackWhite.jsx
+++ src/Pages/BlackWhite.jsx
@@ -1,10 +1,22 @@
 import { bnw } from '../data'
+import { motion } from 'framer-motion'
+
 // import LazyLoad from 'react-lazy-load'
+const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] }
+
 const BlackWhite = () => {
   return (
     <div>
       {bnw.map((data) => (
-        <img src={data} alt="..." loading="lazy" width="100%" height="100%" />
+        <motion.img
+          whileHover={{ scale: 0.95 }}
+          transition={transition}
+          src={data}
+          alt="..."
+          loading="lazy"
+          width="100%"
+          height="100%"
+        />
       ))}
     </div>
   )
